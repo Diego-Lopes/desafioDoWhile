3bfa64f622306281c01fdffb545058f64ee814bb
Diego-Lopes <diegolopes2009@hotmail.com>
2021-12-05 18:02:49 -0300

    imp: news components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import { About } from "@/layouts/About";
+import { Details } from "@/layouts/Details";
 import { HomeLayout } from "@/layouts/Home";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import React from "react";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +12,12 @@ const Home: NextPage = () => {
       <main>
         <HomeLayout />
       </main>
+      <section>
+        <About />
+      </section>
+      <section>
+        <Details />
+      </section>
     </>
   );
 };
