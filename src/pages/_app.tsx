e25e18d1f0c270ca9622922a8ae5cb940d1201b6
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-12-06 12:24:19 -0300

    refactore: added ToastContainer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,22 @@
 import { GlobalStyle, theme } from "@/styles";
 import type { AppProps } from "next/app";
 import React from "react";
+import { Bounce, ToastContainer } from "react-toastify";
 import { ThemeProvider } from "styled-components";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <ToastContainer
+          transition={Bounce}
+          limit={2}
+          theme="dark"
+          autoClose={3000}
+          newestOnTop={false}
+          rtl={false}
+        />
         <Component {...pageProps} />
         <GlobalStyle />
       </ThemeProvider>
