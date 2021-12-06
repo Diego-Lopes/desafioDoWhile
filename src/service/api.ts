ea1d6555b2f818137ebf395563f6854c6a0cb544
Diego-Lopes <diegolopes2009@hotmail.com>
2021-12-06 13:33:38 -0300

    refactore: modification link api

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://sheet.best/api/sheets/d4c6f8e1-769b-4c7b-8737-c3a66a96d497",
+  baseURL: "https://sheet.best/api/sheets/a7e04803-bdbf-4a2a-8d6b-84b80d0b9e27",
 });
