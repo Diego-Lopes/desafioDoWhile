4cfc9701bf41e4df4b5d868dfabae0a9e0827656
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-12-06 17:35:10 -0300

    refactore: added meta and link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,9 +42,23 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="canonical" href="https://desafio-do-while.vercel.app/" />
+          <link
+            rel="icon"
+            href="https://cdn-icons.flaticon.com/png/128/3330/premium/3330314.png?token=exp=1638821370~hmac=e3dddee910069d56fc7dd36aeaa9659e"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;600;700&display=swap"
             rel="stylesheet"
+          />
+
+          <meta
+            name="author"
+            content="NetFamilia | Made whit ❤ by Diego Lopes"
+          />
+          <meta
+            name="description"
+            content="NetFamilia tem o intuito de facilitar o acesso a educação por meio da tecnoligia atual."
           />
         </Head>
         <body>
