5ba62788801429220acf68f0ab61119b733bd2e7
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-12-06 17:35:36 -0300

    refactore: added library Aos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { GlobalStyle, theme } from "@/styles";
 import type { AppProps } from "next/app";
-import React from "react";
+import React, { useEffect } from "react";
 import { Bounce, ToastContainer } from "react-toastify";
 import { ThemeProvider } from "styled-components";
 import "react-toastify/dist/ReactToastify.css";
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
   return (
     <>
       <ThemeProvider theme={theme}>
