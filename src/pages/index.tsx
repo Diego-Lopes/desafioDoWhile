9c4c7ba3ea2d4ce30f3f062f55802379316626f6
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-12-07 12:20:14 -0300

    refactore: added title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,14 @@ import { Form } from "@/layouts/Form";
 import { HomeLayout } from "@/layouts/Home";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import React from "react";
 
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>NetFamilia | Programa educação para todos</title>
+      </Head>
       <main>
         <HomeLayout />
       </main>
