941222e14bbe4e80c9917beb0af113ec8d98bc1a
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-12-29 17:52:33 -0300

    chore: added tag mananger

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -106,8 +106,27 @@ export default class MyDocument extends Document {
           />
 
           {/* END CARD */}
+          {/* tag manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-THL98ZT');`,
+            }}
+          />
+          {/* end tag */}
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-THL98ZT"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
