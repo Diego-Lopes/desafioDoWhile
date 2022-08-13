47f45d3256cd65bc34612826589d3f8ae2b8d752
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2022-08-13 00:49:58 -0400

    chore: modification seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -117,6 +117,16 @@ export default class MyDocument extends Document {
               })(window,document,'script','dataLayer','GTM-THL98ZT');`,
             }}
           />
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-NS4G8F2C3K" />
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NS4G8F2C3K');
+        </script>
           {/* end tag */}
         </Head>
         <body>
